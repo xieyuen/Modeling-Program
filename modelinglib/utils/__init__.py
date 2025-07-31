bafbccc97b1a2baf9c251665e5fda69fb695a87b
xieyuen <xieyuenol@outlook.com>
2025-07-31 23:58:23 +0800

    Update __init__.py

--- modelinglib/utils/__init__.py
+++ modelinglib/utils/__init__.py
@@ -1,13 +1,9 @@
-from typing import Iterable
-
 import numpy as _np
 from pandas import _DataFrame, _Series
 from scipy.stats import t as _t
 
-from .normalization import Normalize
 
-
-def remove(data: _DataFrame, condition: _Series | Iterable):
+def remove(data: _DataFrame, condition: _Series):
     """
     删除符合条件的行，会直接修改数据
 
@@ -20,7 +16,7 @@ def remove(data: _DataFrame, condition: _Series | Iterable):
     """
     data.drop(
         data[condition].index,
-        inplace=True
+        inplace=True,
     )
 
 
@@ -31,7 +27,7 @@ def remove_na(data: _DataFrame):
     for subset in data:
         data.dropna(
             subset=subset,
-            inplace=True
+            inplace=True,
         )
 
 
@@ -39,7 +35,7 @@ def r_squared(model, X, y):
     return model.score(X, y)
 
 
-def adjusted_r_squared(r2, X):
+def adjusted_r_squared(X, r2):
     # 计算调整R²
     n, p = X.shape  # 样本数和特征数
     adjusted_r2 = 1 - (1 - r2) * (n - 1) / (n - p - 1)
@@ -67,34 +63,33 @@ def p(model, X, y):
 
 
 def related_r(x, y):
-    """样本相关系数"""
-    length = len(x)
+    r"""
+    样本相关系数
 
-    x_mean = x.mean()
-    y_mean = y.mean()
+    计算公式(Latex代码)
+    r = \frac{
+        \sum_{i=1}^{n}
+            x_i y_i - n\bar{x}\bar{y}
+    }{
+        \sqrt{\sum_{i=1}^{n}x_i^2-n\bar{x}^2}
+        \sqrt{\sum_{i=1}^{n}y_i^2-n\bar{y}^2}
+    }
 
-    return (
-        (_np.sum(
-            [xi*yi for xi, yi in zip(x, y)]
-            ) - length * x_mean * y_mean)
-        /
-        (_np.sqrt(
-            _np.sum([xi**2 for xi in x]) - length * x_mean ** 2
-        ) * _np.sqrt(
-            _np.sum([yi**2 for yi in y]) - length * y_mean ** 2
-        ))
-    )
+    Args:
+        x (SeriesLike): 自变量的数据
+        y (SeriesLike): 因变量的数据
+    """
+    n = len(x)  # n
+    x_mean = x.mean()  # \bar{x}
+    y_mean = y.mean()  # \bar{y}
 
+    sum_xiyi = _np.sum(xi*yi for xi, yi in zip(x, y))
+    sum_xi2 = _np.sum(xi ** 2 for xi in x)
+    sum_yi2 = _np.sum(yi ** 2 for yi in y)
 
-def save(model, file_path):
-    if hasattr(model, "save"):
-        model.save()
-        return
-    
-    import pickle
-
-    with open(file_path, "wb") as f:
-        pickle.dump(model, f)
+    return (sum_xiyi - n * x_mean * y_mean) / (_np.sqrt(
+        (sum_xi2 - n * x_mean ** 2) * (sum_yi2 - n * y_mean ** 2)
+    ))
 
 
 def print_result_for_lm(model, x, y):
