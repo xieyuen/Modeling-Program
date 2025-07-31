0ce49236ccec1b2f846935409b26010ec1aa909d
xieyuen <xieyuenol@outlook.com>
2025-07-31 23:58:15 +0800

    Update normalization.py

--- modelinglib/utils/normalization.py
+++ modelinglib/utils/normalization.py
@@ -1,20 +1,74 @@
 import pandas as pd
 
-class Normalize:
+class Normalizer:
+    """Normalizer, abstract class, init for fiting"""
     def __init__(self, data: pd.DataFrame):
-        self.data: pd.DataFrame = data
+        self.original = data
+        self.max = data.max()
+        self.min = data.min()
+        self.std = data.std()
+        self.mean = data.mean()
+        self.length = self.max - self.min
 
-    def z_score(self):
-        return (self.data - self.data.mean()) / self.std()
+    def normalize(self, data: pd.DataFrame | None = None) -> pd.DataFrame:
+        raise NotImplementedError
 
-    def max_min(self, feature_range=(0, 1)):
-        min_vals = self.data.min()
-        max_vals = self.data.max()
-        ranges = max_vals - min_vals
-        ranges[ranges == 0] = 1  # 防止全部相同的数据导致报错
+class ZScoreNormalizer(Normalizer):
+    """Z-score normalizer"""
+    def __normalize(self, data):
+        return (data - self.mean) / self.std 
+
+    def normalize(self, data=None):
+        if data is None:
+            if not hasattr(self, "normalized"):
+                self.normalized = self.__normalize(self.original)
         
-        # 标准化到 [0, 1]
-        normalized = (self.data - min_vals) / ranges
+            return self.normalized
+        return self.__normalize(data)
 
-        a, b = feature_range
-        return normalized * (b - a) + a  # 放缩到 [a, b]
+
+class MinMaxTargetRange:
+    def __init__(self):
+        self.length, self.max, self.min = None, None, None
+        self.is_initialized = False
+
+    def set(self, min, max):
+        assert max > min
+
+        self.min = min
+        self.max = max
+        self.length = max - min
+        self.is_initialized = True
+
+
+class MinMaxNormalizer(Normalizer):
+    def __init__(self, data):
+        super().__init__(data)
+        self.range = MinMaxTargetRange()
+    
+    def __normalize(self,data):
+        self.length[self.length == 0] = 1  # 防止全为某一常数
+        # (b - a)(x - min)/(max - min) + a
+        return (data - self.min) * self.range.length / self.length + self.range.min
+
+    def normalize(self, data=None):
+        if not self.has_set_range:
+            raise RuntimeError("MinMax标准化需要指定映射范围")
+
+        if data is None:
+            if not hasattr(self, "normalized"):
+                self.normalized = self.__normalize(self.original)
+            return self.normalized
+        return self.__normalize(data)
+
+    @property
+    def has_set_range(self):
+        return self.range.is_initialized
+
+    def set_range(self, min, max):
+        self.range.set(min, max)
+        return self
+
+
+class RobustNormalizer(Normalizer):
+    pass
